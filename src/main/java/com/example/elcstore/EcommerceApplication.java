d3cac684fb8f6960eab83737542ff34253d7013a
Ilkin Agayev <agayevilkin76@gmail.com>
2023-11-11 07:22:14 +0400

    ADDED login and token page (html)

--- src/main/java/com/example/elcstore/EcommerceApplication.java
+++ src/main/java/com/example/elcstore/EcommerceApplication.java
@@ -2,10 +2,21 @@ package com.example.elcstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class EcommerceApplication {
+    @GetMapping("/login")
+    public String login() {
+        return "loginpage";
+    }
 
+    @GetMapping("/token")
+    public String getToken() {
+        return "/tokenpage";
+    }
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
